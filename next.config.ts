a6251e26ece17ab9b527d41cf74420b87557ae1d
Lyndon Baylin Jr. <lyndon@syntacticsinc.com>
2025-10-05 13:07:22 +0800

    chore:  added pre-configured settings

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,50 @@ import { type NextConfig } from 'next';
 import { env } from './env.mjs';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* Enable React Strict Mode */
   reactStrictMode: true,
+
+  /* Eslint config */
+  eslint: {
+    // ✅ disable build-time linting (we enforce elsewhere)
+    ignoreDuringBuilds: true,
+    // optional: explicitly tell Next which dirs to lint if you re-enable
+    dirs: ['src'],
+  },
+
+  /* Config option for allowed origin regarding CORS concern */
+  allowedDevOrigins: [
+    'localhost:3000',
+    '127.0.0.1:3000', // Playwright uses this
+  ],
+
+  /*
+    Config option for optimizing package imports.
+    This will only load the modules that are actually use in the app
+
+    https://nextjs.org/docs/advanced-features/optimizing-packages
+
+    Uncomment the line below and provide the package name you want to optimize
+  */
+  // optimizePackageImports: [],
+
+  /*
+    Config option for remote access of external files
+
+    https://nextjs.org/docs/app/getting-started/images
+
+    Uncomment the line below and provide the config details of the remote access
+  */
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'placehold.co',
+  //       port: '',
+  //       pathname: '/**',
+  //     },
+  //   ],
+  // },
 };
 
 export default env.ANALYZE ? withBundleAnalyzer({ enabled: env.ANALYZE })(nextConfig) : nextConfig;
