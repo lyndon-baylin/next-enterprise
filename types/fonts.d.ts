126a1403678f4eaa80ec1fcc662ed8625d6e8bdb
Lyndon Baylin Jr. <lyndon@syntacticsinc.com>
2025-10-05 14:09:17 +0800

    fix: shim mismatch

--- types/fonts.d.ts
+++ types/fonts.d.ts
@@ -1,11 +1,19 @@
+// types/fonts.d.ts
+// This is a shim file for Next.js’ built‑in font system (and Vercel’s geist/font/*) use virtual modules that don’t exist on disk,
+// so ESLint (and especially TypeScript’s resolver) can’t find them unless you give it a hint.
+
+// To make this work, we need to tell ESLint and TypeScript know these modules exist.
+// The `types` directory must be defined in the `tsconfig.json` file `include` props for automatic type definition pickup.
 declare module 'geist/font/sans' {
-  export const GeistSans: (options: { subsets: string[]; weight?: string[]; display?: string; variable?: string }) => {
+  export const GeistSans: {
     className: string;
+    variable: string;
   };
 }
 
 declare module 'geist/font/mono' {
-  export function GeistMono(options: { subsets: string[]; weight?: string[]; display?: string; variable?: string }): {
+  export const GeistMono: {
     className: string;
+    variable: string;
   };
 }
