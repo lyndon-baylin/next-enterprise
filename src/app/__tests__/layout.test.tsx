d0307d74a4ce26d665806a51d72897ca895174b1
Lyndon Baylin Jr. <lyndon@syntacticsinc.com>
2025-10-11 22:15:38 +0800

    chore: mock root layout component

--- src/app/__tests__/layout.test.tsx
+++ src/app/__tests__/layout.test.tsx
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import { vi } from 'vitest';
 
+import { GeistMono, GeistSans } from '@/fonts';
+
 vi.mock('geist/font/sans', () => ({
   GeistSans: () => ({
     className: 'geistSans',
@@ -15,7 +17,19 @@ vi.mock('geist/font/mono', () => ({
   }),
 }));
 
-import { RootLayoutBody } from '../layout';
+/**
+ * A root layout body component that wraps the children in a div with Geist fonts.
+ * It applies the GeistSans and GeistMono font variables to the div.
+ * It also applies the antialiased class to the div.
+ * @param {object} props - The props object.
+ * @param {React.ReactNode} props.children - The children to render.
+ * @returns {React.ReactElement} - The root layout body component.
+ *
+ * @note This only exist to satisfy the layout testing.
+ */
+function RootLayoutBody({ children }: Readonly<{ children: React.ReactNode }>): React.ReactElement {
+  return <div className={`${GeistSans.variable} ${GeistMono.variable} antialiased`}>{children}</div>;
+}
 
 test('Renders layout with children', () => {
   render(
