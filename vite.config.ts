68bb9a0a0f36b51182f0a9d6d79ff85c25e8f484
Lyndon Baylin Jr. <4katsuki.pain@gmail.com>
2026-03-22 19:09:23 +0800

    fix(scripts): fix storybook test

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+// vite.config.ts
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [
+    tsconfigPaths({
+      projectDiscovery: 'lazy',
+      logFile: false,
+    }),
+    react(),
+  ],
+});
